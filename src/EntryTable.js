d05d2eda6d917855e59a5132f6d930ff96a776c6
Ayushman Sinha <ayushman.sinhaa@gmail.com>
2022-01-10 23:29:54 +0530

    StackFix

--- src/EntryTable.js
+++ src/EntryTable.js
@@ -1,55 +1,74 @@
 import React,{useState} from 'react';
-import NumericInput from 'react-numeric-input';
 import './tableEdit.css';
 const EntryTable = (props) => {
-    const entry=props.numOfEntries;
-    const[inputArrival,SetinputArrival]=useState('');
-    const[inputBurst,SetinputBurst]=useState('');
- 
-    function changeArrival(e)
-    {
-       
-           
-        console.log(e.target.value+"A");
-        /*
-        if(!Number(e.target.value))
-       return;
-        else*/
-        SetinputArrival(e.target.value);
-    }
-    function changeBurst(e)
-    {
-        console.log(e.target.value+"B"); 
-        SetinputBurst(e.target.value);
+    const entry = props.numOfEntries;
+    const [inputArrival, setInputArrival] = useState(Array(entry).fill(""));
+    const [inputBurst, setInputBurst] = useState(Array(entry).fill(""));
+  
+    function changeArrival(index) {
+      return (e) => {
      
+       // console.log(e.target.value + "A");
+        setInputArrival((values) =>
+          values.map((value, i) => (i === index ? e.target.value : value))
+        );
+      
+      };
     }
-
-   
-   // console.log(entry);
-    var ArrayEntry=[];
-    for(var i=1;i<=entry;i++)
-    ArrayEntry.push( <tr key={i}>                       
-        <td className='row_editContent' >P{i}</td>
-        <td className='row_editContent'><input className='input_edit' placeholder='0' type='number' value={inputArrival} onChange={(e)=>changeArrival(e)}></input>ms</td>
-        <td className='row_editContent'><input  className='input_edit' placeholder='0' type='number' value={inputBurst} onChange={(e)=>changeBurst(e)}></input>ms</td>
-        </tr>);
-
-        return (
-        <div>
-            <table className='MainTableContainer' >
-                <thead>
-                    <tr >
-                        <th className='row_editHeading'>Process</th>
-                        <th className='row_editHeading'>Arrival Time</th>
-                        <th className='row_editHeading'>Burst Time</th>
-                    </tr>
-                </thead>
-                <tbody> 
-                    {ArrayEntry}
-                </tbody>
-            </table>
-        </div>
-    )
-}
-
+    function changeBurst(index) {
+      return (e) => {  
+      //  console.log(e.target.value + "B");
+     
+        setInputBurst((values) =>
+          values.map((value, i) => (i === index ? e.target.value : value))
+        );
+      };
+    }
+  
+    // console.log(entry);
+    const ArrayEntry = Array.from({ length: entry}).map((_, i) => (
+      <tr key={i}>
+          <td className="row_editContent">P{i}</td>
+          <td className="row_editContent">
+            <input
+              className="input_edit"
+              placeholder="0"
+              type="number"
+              value={inputArrival[i]}
+              onChange={changeArrival(i)}
+              min={0}
+              max={50}
+            />
+            ms
+          </td>
+          <td className="row_editContent">
+            <input
+              className="input_edit"
+              placeholder="0"
+              type="number"
+              value={inputBurst[i]}
+              onChange={changeBurst(i)}
+              min={0}
+              max={50}
+            />
+            ms
+          </td>
+        </tr>
+    ));
+  
+    return (
+      <div>
+        <table className="MainTableContainer">
+          <thead>
+            <tr>
+              <th className="row_editHeading">Process</th>
+              <th className="row_editHeading">Arrival Time</th>
+              <th className="row_editHeading">Burst Time</th>
+            </tr>
+          </thead>
+          <tbody>{ArrayEntry}</tbody>
+        </table>
+      </div>
+    );
+  };
 export default EntryTable
